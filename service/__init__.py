81ddd4eda9ae140b97b4150c37102463f7970b9b
Yannick Mounguengui <angeyannick.mounguenguiitoua.etu@univ-lille.fr>
2024-12-28 19:06:57 -0800

    Add Security to Your RESTful Service

--- service/__init__.py
+++ service/__init__.py
@@ -1,11 +1,10 @@
 """
-Package: service
-Package for the application models and service routes
-This module creates and configures the Flask app and sets up the logging
-and SQL database
+Application Setup
 """
 import sys
 from flask import Flask
+from flask_talisman import Talisman
+from flask_cors import CORS
 from service import config
 from service.common import log_handlers
 
@@ -13,25 +12,27 @@ from service.common import log_handlers
 app = Flask(__name__)
 app.config.from_object(config)
 
-# Import the routes After the Flask app is created
-# pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
-from service import routes, models  # noqa: F401 E402
+# Apply Security Headers
+talisman = Talisman(app)
 
-# pylint: disable=wrong-import-position
-from service.common import error_handlers, cli_commands  # noqa: F401 E402
+# Enable CORS
+CORS(app)
 
-# Set up logging for production
+# Import routes and models
+from service import routes, models
+from service.common import error_handlers, cli_commands
+
+# Logging Configuration
 log_handlers.init_logging(app, "gunicorn.error")
 
 app.logger.info(70 * "*")
-app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info("  ACCOUNT SERVICE RUNNING ".center(70, "*"))
 app.logger.info(70 * "*")
 
 try:
-    models.init_db(app)  # make our database tables
-except Exception as error:  # pylint: disable=broad-except
-    app.logger.critical("%s: Cannot continue", error)
-    # gunicorn requires exit code 4 to stop spawning workers when they die
+    models.init_db(app)
+except Exception as error:
+    app.logger.critical(f"Database Initialization Error: {error}")
     sys.exit(4)
 
 app.logger.info("Service initialized!")
